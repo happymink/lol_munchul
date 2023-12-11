45362cffa05e0b02644b35da88776da3104665e9
happymink <minkyun@kakao.com>
2023-12-11 15:43:48 +0900

    Initial commit

---
+++ src/main/java/com/service/lol_munchul/LolMunchulApplication.java
@@ -0,0 +1,13 @@
+package com.service.lol_munchul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolMunchulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolMunchulApplication.class, args);
+	}
+
+}
